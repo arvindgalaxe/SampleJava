0482c5a2375fca6fc06f0fcfc8b5a1275cc8d9f7
akatte <akatte@GBWK-INA103SNDQ.galaxy.lan>
2017-06-22 18:16:21 +0530

    Initial check-in Sample Java

---
+++ SampleJava/src/com/katte/galaxe/DemoClass.java
@@ -0,0 +1,11 @@
+package com.katte.galaxe;
+
+import java.util.Date;
+//arvind katte
+public class DemoClass {
+	public static void main(String[] args) {
+		System.out.println("==================================");
+		Date today = new Date();
+		System.out.println("Today is :" + today);
+	}
+}
